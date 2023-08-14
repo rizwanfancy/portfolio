a898ff3a14c891d12c21a63af2a0b7caed7979bd
HOME-LAPTOP\rizwan <rizwanfancy@hotmail.com>
2023-08-14 19:43:05 +0500

    master

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import * as AOS from 'aos';
+import { Title, Meta } from '@angular/platform-browser';
+import { LanguageService } from "src/app/services/language/language.service"
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rizwan-portfolio';
+
+  constructor(
+    private titleService: Title,
+    private metaService: Meta,
+    private languageService: LanguageService
+  ) {
+  }
+  ngOnInit(): void {
+
+    this.languageService.initLanguage()
+
+    this.titleService.setTitle("Rizwan Anwer | Full-Stack Developer");
+    this.metaService.addTags([
+      { name: 'keywords', content: 'Frontend, MEAN Stack Developer , MERN Stack Development, Full-Stack Developer, Software Engineer, software, developer' },
+      { name: 'description', content: 'As a software engineer with expertise in both MEAN and MERN stacks, I have a comprehensive understanding of web development. My strong foundation in JavaScript allows me to effectively work with front-end technologies such as Angular,VueJs and React, as well as back-end technologies such as C#, Node.js and Express.' },
+    ]);
+    
+    AOS.init();
+  }
+}
