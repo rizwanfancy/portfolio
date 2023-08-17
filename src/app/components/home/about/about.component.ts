a14c6d25bd2fb8b386dc4d73f0aba210d5f51b22
HOME-LAPTOP\rizwan <rizwanfancy@hotmail.com>
2023-08-17 18:56:07 +0500

    update skills list

--- src/app/components/home/about/about.component.ts
+++ src/app/components/home/about/about.component.ts
@@ -8,6 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   paragraphs: string[] = [`Experienced Full Stack Developer with a demonstrated history of <span class='underline'> 8+ years </span> in the software development industry. Skilled in a variety of technologies including <span class='underline'> VueJs, Angular, React, JavaScript, TypeScript, and .Net Core </span>. Proficient in both front-end and back-end development, with a strong focus on performance enhancement and code optimization. I have successfully led teams to achieve technical and management goals, delivering innovative solutions that have a positive impact. My expertise spans across API development, deployment, and utilizing various UI kits such as <span class='underline'> Aggrid, Devexpress, Bootstrap, and Material UI </span>. I am well-versed in version control systems like Github and Gitlab, as well as project management tools like Jira. Always eager to learn new technologies and continuously improve existing code.`]
+  skills: string[] =
+    [
+      '.Net Core',
+      'Nodejs',
+      'Angular',
+      'VueJs',
+      'React',
+      'JavaScript',
+      'TypeScript',
+      'HTML/CSS',
+      'Linq',
+      'ExpressJs',
+      'SQL',
+      'MongoDb',
+      'Entity Framework',
+      'Dapper/Telerik'
+    ]
+
+  otherSkills: string[] =
+    [
+      'REST Development',
+      'Deployment',
+      'IIS',
+      'Redis',
+      'Aggrid',
+      'Devexpress Ui Kit',
+      'Bootstrap',
+      'Material',
+      'Github',
+      'Gitlab',
+      'Azure',
+      'Jira',
+    ]
   constructor(
   ) { }
 
